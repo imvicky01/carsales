64a9ceeaf83f304ee0cd4c1bdcf179f121c07ca1
vicky01 <imspider01@gmail.com>
2023-09-13 16:57:19 +0000

    navbar open close finsh

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Home.css";
 
+import homeImage from '../../Assets/4.png'
+
 const Home = () => {
-  return <div className=""></div>;
+  return <div className="home">
+    <div className="secContainer">
+      <dir className="homeText">
+        <span className="homeSpan">
+          Meet your new car
+        </span>
+        <h1 className="homeTitle">
+          Honda Civic Type R
+        </h1>
+        <div className="btns flex">
+          <button className="btn">More Details</button>
+          <button className="btn primaryBtn">Test Drive</button>
+        </div>
+      </dir>
+      <div className="homeImage">
+        <img src={homeImage} alt="home image" />
+
+      </div>
+    </div>
+  </div>;
 };
 
 export default Home;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,46 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from '../../Assets/logo.png'
 import { IoIosCloseCircle } from 'react-icons/io'
 import { TbGridDots } from 'react-icons/tb'
 
 const Navbar = () => {
+
+  const [navbar, setnavbar] = useState('navbar')
+
+  const showNavbar = () => {
+    setnavbar('navbar')
+  }
+
+  const removeNavbar = () => {
+    setnavbar('navbar')
+  }
+
   return (
     <div className="header">
       <div className="logoDiv">
         <img src={logo} alt="logo image" className="logo" />
       </div>
-      <div className="navbar">
+      <div className={navbar}>
         <ul className="menu">
-          <li className="linkItem">
+          <li onClick={removeNavbar} className="linkItem">
             <a href="/" className="link">
               Used Car
             </a>
           </li>
-          <li className="linkItem">
+          <li onClick={removeNavbar} className="linkItem">
             <a href="/" className="link">
               New Cars
             </a>
           </li>
-          <li className="linkItem">
+          <li onClick={removeNavbar} className="linkItem">
             <a href="/" className="link">
               Actions
             </a>
           </li>
-          <li className="linkItem">
+          <li onClick={removeNavbar} className="linkItem">
             <a href="/" className="link">
               Seller
             </a>
           </li>
         </ul>
 
-        <IoIosCloseCircle className='icon 
-        closeIcon' />
+        <IoIosCloseCircle className='icon closeIcon' onClick={removeNavbar} />
       </div>
       <div className="signUp flex">
         <div className="text">Sign Up</div>
-        <TbGridDots className='icon 
-      toggleNavbarIcon' />
+        <TbGridDots className='icon toggleNavbarIcon' onClick={showNavbar} />
       </div>
     </div>
   );
