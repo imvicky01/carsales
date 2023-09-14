b77c66ee59c23c0c7702e9078ca3685c1da89438
vicky01 <imspider01@gmail.com>
2023-09-14 16:58:10 +0000

    review jsx la xtra user varikum add panne irrukan

--- src/Components/Auction/Auction.jsx
+++ src/Components/Auction/Auction.jsx
@@ -5,6 +5,11 @@ import { BsArrowLeftShort } from "react-icons/bs";
 import { BsArrowRightShort } from "react-icons/bs";
 
 import car1 from "../../Assets/1.png";
+import car2 from "../../Assets/2.png";
+import car3 from "../../Assets/8.png";
+import car4 from "../../Assets/9.png";
+import car5 from "../../Assets/10.png";
+import car6 from "../../Assets/11.png";
 
 const Auction = () => {
   return (
@@ -19,7 +24,7 @@ const Auction = () => {
           </div>
         </div>
 
-        <div className="carContainer grid">
+        <div className="carContainer grid singleCarActive">
           <div className="singleCar grid">
             <div className="imgDiv">
               <img src={car1} alt="Car Image" />
@@ -28,9 +33,79 @@ const Auction = () => {
             <span className="miles">11457 Miles</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
-            <div className="price_seller flex">
+            <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="seller">Best seller</span>
+              <span className="buybtn">buy now</span>
+            </div>
+          </div>
+
+          <div className="singleCar grid singleCarActive">
+            <div className="imgDiv">
+              <img src={car2} alt="Car Image" />
+            </div>
+            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
+            <span className="miles">11457 Miles</span>
+            <span className="AWD">AWD 4-Cylinder Turbo</span>
+
+            <div className="price_buyBtn flex">
+              <span className="price">$41,200</span>
+              <span className="buybtn">buy now</span>
+            </div>
+          </div>
+
+          <div className="singleCar grid singleCarActive">
+            <div className="imgDiv">
+              <img src={car3} alt="Car Image" />
+            </div>
+            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
+            <span className="miles">11457 Miles</span>
+            <span className="AWD">AWD 4-Cylinder Turbo</span>
+
+            <div className="price_buyBtn flex">
+              <span className="price">$41,200</span>
+              <span className="buybtn">buy now</span>
+            </div>
+          </div>
+
+          <div className="singleCar grid singleCarActive">
+            <div className="imgDiv">
+              <img src={car4} alt="Car Image" />
+            </div>
+            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
+            <span className="miles">11457 Miles</span>
+            <span className="AWD">AWD 4-Cylinder Turbo</span>
+
+            <div className="price_buyBtn flex">
+              <span className="price">$41,200</span>
+              <span className="buybtn">buy now</span>
+            </div>
+          </div>
+
+          <div className="singleCar grid singleCarActive">
+            <div className="imgDiv">
+              <img src={car5} alt="Car Image" />
+            </div>
+            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
+            <span className="miles">11457 Miles</span>
+            <span className="AWD">AWD 4-Cylinder Turbo</span>
+
+            <div className="price_buyBtn flex">
+              <span className="price">$41,200</span>
+              <span className="buybtn">buy now</span>
+            </div>
+          </div>
+
+          <div className="singleCar grid singleCarActive">
+            <div className="imgDiv">
+              <img src={car6} alt="Car Image" />
+            </div>
+            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
+            <span className="miles">11457 Miles</span>
+            <span className="AWD">AWD 4-Cylinder Turbo</span>
+
+            <div className="price_buyBtn flex">
+              <span className="price">$41,200</span>
+              <span className="buybtn">buy now</span>
             </div>
           </div>
         </div>
--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,8 +1,162 @@
 import React from "react";
 import "./Review.css";
+import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
+import { AiFillStar } from 'react-icon/ai'
+
+import car1 from '../../Assets/5.png'
+import car2 from '../../Assets/7.png'
+import car3 from '../../Assets/8.png'
+
+import user1 from '../../Assets/6.png'
+import user2 from '../../Assets/10.png'
+import user3 from '../../Assets/11.png'
+
 
 const Review = () => {
-  return <div className=""></div>;
+  return <div className="review section">
+    <div className="secContainer container">
+      <div className="secHeading flex">
+        <h3 className="secTitle">
+          Recent Reviews
+        </h3>
+        <div className="navBtns flex">
+          <BsArrowLeftShort className='icon leftIcon' />
+          <BsArrowRightShort className='icon rightIcon' />
+        </div>
+      </div>
+      <div className="reviewContainer grid">
+        <div className="singleReview grid">
+          <div className="imgDiv">
+            <img src={car1} alt="Car Image" />
+          </div>
+          <h5 className="reviewTitle">
+            2023 Cadiallac
+          </h5>
+          <span className="desc">
+            Good cars are safe, fast, affordable to
+            purchase, economical to operate,reliable, capapcios, comfortable and attractive.
+          </span>
+
+          <div className="reviewweDiv flex">
+            <div className="leftDiv flex">
+              <div className="reviewerImage">
+                <img src={user1} alt="Reviewer Image" />
+              </div>
+              <div className="aboutReviewer">
+                <span className="name">
+                  Jakline Nelson
+                </span>
+                <p>Chief Editor</p>
+              </div>
+            </div>
+            <div className="rightDiv flex">
+              <AiFillStar className='icon' />
+              <blockquote>4.84</blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="reviewContainer grid">
+        <div className="singleReview grid">
+          <div className="imgDiv">
+            <img src={car1} alt="Car Image" />
+          </div>
+          <h5 className="reviewTitle">
+            2023 Cadiallac
+          </h5>
+          <span className="desc">
+            Good cars are safe, fast, affordable to
+            purchase, economical to operate,reliable, capapcios, comfortable and attractive.
+          </span>
+
+          <div className="reviewweDiv flex">
+            <div className="leftDiv flex">
+              <div className="reviewerImage">
+                <img src={user1} alt="Reviewer Image" />
+              </div>
+              <div className="aboutReviewer">
+                <span className="name">
+                  Jakline Nelson
+                </span>
+                <p>Chief Editor</p>
+              </div>
+            </div>
+            <div className="rightDiv flex">
+              <AiFillStar className='icon' />
+              <blockquote>4.84</blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="reviewContainer grid">
+        <div className="singleReview grid">
+          <div className="imgDiv">
+            <img src={car2} alt="Car Image" />
+          </div>
+          <h5 className="reviewTitle">
+            2023 Cadiallac
+          </h5>
+          <span className="desc">
+            Good cars are safe, fast, affordable to
+            purchase, economical to operate,reliable, capapcios, comfortable and attractive.
+          </span>
+
+          <div className="reviewweDiv flex">
+            <div className="leftDiv flex">
+              <div className="reviewerImage">
+                <img src={user2} alt="Reviewer Image" />
+              </div>
+              <div className="aboutReviewer">
+                <span className="name">
+                  Jakline Nelson
+                </span>
+                <p>Chief Editor</p>
+              </div>
+            </div>
+            <div className="rightDiv flex">
+              <AiFillStar className='icon' />
+              <blockquote>4.84</blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="reviewContainer grid">
+        <div className="singleReview grid">
+          <div className="imgDiv">
+            <img src={car3} alt="Car Image" />
+          </div>
+          <h5 className="reviewTitle">
+            2023 Cadiallac
+          </h5>
+          <span className="desc">
+            Good cars are safe, fast, affordable to
+            purchase, economical to operate,reliable, capapcios, comfortable and attractive.
+          </span>
+
+          <div className="reviewweDiv flex">
+            <div className="leftDiv flex">
+              <div className="reviewerImage">
+                <img src={user3} alt="Reviewer Image" />
+              </div>
+              <div className="aboutReviewer">
+                <span className="name">
+                  Jakline Nelson
+                </span>
+                <p>Chief Editor</p>
+              </div>
+            </div>
+            <div className="rightDiv flex">
+              <AiFillStar className='icon' />
+              <blockquote>4.84</blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Review;
