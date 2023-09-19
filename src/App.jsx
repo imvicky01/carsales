1e6080a9f82bdd6f9b3d27fcb1b676c3c42089b8
vicky01 <imspider01@gmail.com>
2023-09-19 11:26:37 +0530

    still facing error in lightbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Auction from "./Components/Auction/Auction";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
 import Search from "./Components/Search/Search";
 import Trending from "./Components/Trending/Trending";
 import Sellers from "./Components/Sellers/Sellers";
+import Auction from "./Components/Auction/Auction";
 import Review from "./Components/Review/Review";
 import Footer from "./Components/Footer/Footer";
 import NavbarPage from "./Components/NavbarPage/NavbarPage";
@@ -17,34 +17,26 @@ const App = () => {
       <div className="title">
         <Navbar />
         <Routes>
-          {/* Define a parent route for /add */}
-          <Route path="/add" element={<AddPage />}>
-            {/* Nest the child routes for /add */}
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-          </Route>
-          {/* Define other routes */}
-          <Route path="/home" element={<Trending />} />
-          <Route path="/sellers" element={<Sellers />} />
-          <Route path="/auction" element={<Auction />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/navbar" element={<NavbarPage />} />
-          {/* Add more routes as needed */}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/navbar-page" element={<NavbarPage />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
 };
 
-export default App;
-
-// AddPage component to render Home and Search components together
-const AddPage = () => {
+const MainPage = () => {
   return (
     <div>
       <Home />
       <Search />
+      <Trending />
+      <Sellers />
+      <Auction />
+      <Review />
     </div>
   );
 };
+
+export default App;
--- src/Components/NavbarPage/NavbarPage.jsx
+++ src/Components/NavbarPage/NavbarPage.jsx
@@ -1,11 +1,73 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar";
+import React, { useState } from "react";
+import "./NavbarPage.scss"; // Import the SCSS file
+import Image1 from "../../Assets/14.jpg";
+import Image2 from "../../Assets/15.jpg";
+import Image3 from "../../Assets/16.jpg";
+import Image4 from "../../Assets/17.jpg";
+import Lightbox from "./Lightbox.jsx"; // Import the Lightbox component
 
 const NavbarPage = () => {
+  const [showLightbox, setShowLightbox] = useState(false);
+
+  const openLightbox = () => {
+    setShowLightbox(true);
+  };
+
   return (
-    <div>
-      <Navbar /> {/* Include your Navbar component here */}
-      {/* Add other content for the new page */}
+    <div className="product-container">
+      {showLightbox && (
+        <Lightbox
+          images={[Image1, Image2, Image3, Image4]}
+          onClose={() => setShowLightbox(false)}
+        />
+      )}
+      <div className="product-gallery">
+        <div className="gallery-image">
+          <img
+            src={Image1}
+            alt="Product 1"
+            onClick={openLightbox}
+            className="gallery-image-item"
+          />
+        </div>
+        <div className="gallery-image">
+          <img
+            src={Image2}
+            alt="Product 2"
+            onClick={openLightbox}
+            className="gallery-image-item"
+          />
+        </div>
+        <div className="gallery-image">
+          <img
+            src={Image3}
+            alt="Product 3"
+            onClick={openLightbox}
+            className="gallery-image-item"
+          />
+        </div>
+        <div className="gallery-image">
+          <img
+            src={Image4}
+            alt="Product 4"
+            onClick={openLightbox}
+            className="gallery-image-item"
+          />
+        </div>
+      </div>
+      <div className="product-details">
+        <div className="product-image">
+          <img src={Image1} alt="Product" />
+        </div>
+        <div className="product-info">
+          <p>Nanba Car House</p>
+          <h1>Mr. Bean Car</h1>
+          <h4>$5000</h4>
+          <a href="#" className="btn">
+            Call Me
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
